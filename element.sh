587c5a5f2152a756d4082c40e443d723c9814f39
intouch.opec <intouch.opec@gmail.com>
2024-01-11 03:44:04 -0500

    feat: display message not found element

--- element.sh
+++ element.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+echo -e "\n~~~~ Periodic Table ~~~~\n"
 CONDITION=""
 if [[ -z $1 ]] 
 then
   echo -e "Please provide an element as an argument." 
   exit
 fi
-if [[ "$INPUT" =~ ^[0-9]+$ ]]
+
+if [[ "$1" =~ ^[0-9]+$ ]]
 then 
-  CONDITION="atomic_number=$$1"
+  CONDITION="atomic_number=$1"
 else 
-  CONDITION="symbol='$$1' OR name='$$1'"
+  CONDITION="symbol='$1' OR name='$1'"
 fi
-echo $WHERE
+
 RESULT_ELEMENT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE $CONDITION;")
+
+if [[ -z $RESULT_ELEMENT ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
